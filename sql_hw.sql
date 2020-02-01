c286de771950934dbbd2e38d41a49a8b160ed376
Nareman Dabbas <naremands@Naremans-MBP.lan1>
2020-02-01 16:14:57 -0600

    Update my file

--- sql_hw.sql
+++ sql_hw.sql
@@ -8,24 +8,27 @@ select first_name ,last_name
 from actor;
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 select upper (concat (a.first_name , ' ' , a.last_name)) as "Actor Name"
-from actor a ;
+from actor as a ;
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
 select a.actor_id ,a.first_name ,a.last_name 
-from actor a 
-where first_name like  "joe";
+from actor as a  
+where a.first_name like  "joe";
 -- 2b. Find all actors whose last name contain the letters `GEN`:
-select * from actor 
-where last_name like '%GEN%';
+select a.first_name ,a.last_name 
+from actor as a  
+where a.last_name like '%GEN%';
 -- 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, in that order:
-select * from actor 
-where last_name like '%LI%'
+select a.first_name ,a.last_name 
+from actor as a  
+where a.last_name like '%LI%'
 ORDER BY TRIM(Last_name) ASC
        , TRIM(First_name) ASC;
 -- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:       
 select country_id,country from country 
 where country IN ('Afghanistan','Bangladesh','China');
-alter table actor 
+
 -- 3a.  create a column in the table `actor` named `description` and use the data type `BLOB`.
+alter table actor 
 add description blob;
 select * from actor ;
 -- 3b. Delete the `description` column.
@@ -33,7 +36,7 @@ alter table actor
 drop column description;
 select * from actor ;
 -- 4a.List the last names of actors, as well as how many actors have that last name.
-select 	last_name as'last name ', count(last_name) as 'last name count'
+select 	last_name as'last name', count(last_name) as 'last name count'
 from actor 
 group by last_name;
 -- 4b. List last names of actors and the number of actors who have that last name,
@@ -72,7 +75,8 @@ join address as a
 on s.address_id = a.address_id;
 -- 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005.
 --  Use tables `staff` and `payment`.
-select concat(s.first_name ,' ' ,s.last_name) as 'staff member' , sum(p.amount) as 'total amount'
+select concat(s.first_name ,' ' ,s.last_name) as 'staff member' ,
+ sum(p.amount) as 'total amount rung '
 from staff as s
 join payment as p 
 on s.staff_id = p.staff_id 
@@ -111,7 +115,8 @@ and f.title like 'K%' or 'Q%' ;
     select concat(a.first_name ,' ',a.last_name)as 'Actors in Alone trip'
     from actor as a 
     where a.actor_id in 
-    (select actor_id from film_actor where film_id = (select film_id from film where title = 'Alone Trip'));
+    (select actor_id from film_actor where film_id
+    = (select film_id from film where title = 'Alone Trip'));
 -- 7c. You need the names and email addresses of all Canadian customers. 
 select concat (c.first_name ,' ',c.last_name )as 'customer names',c.email
 from customer as c 
